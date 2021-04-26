2fb6c4ad27fe04d3cec28cef31d3d276b11ff771
MahzyarMomeni <mahzyar.momeni@gmail.com>
2021-04-26 17:30:42 +0430

    master-update keys file to add database

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,4 +1,9 @@
 module.exports = {
     PORT: process.env.PORT,
+    MYSQL_HOST: process.env.MYSQL_HOST,
+    MYSQL_USER: process.env.MYSQL_USER,
+    MYSQL_PASSWORD: process.env.MYSQL_PASSWORD,
+    MYSQL_DATABASE: process.env.MYSQL_DATABASE,
     NODE_ENV: process.env.NODE_ENV
+
 }
