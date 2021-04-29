8ee4815e1d54b21c3bd68020af9a9b07bfcff5fa
MahzyarMomeni <mahzyar.momeni@gmail.com>
2021-04-29 19:43:27 +0430

    master-add user table

--- DataBase/user.sql
+++ DataBase/user.sql
@@ -1,1 +1,1 @@
-INSERT INTO `user` (`id`, `username`, `email`, `phoneNumber`, `password`) VALUES (1, 'Mahzyar', 'Mahzyar.momeni@gmail.com', '09334328703', '123456789');
+INSERT INTO `user` (`id`, `username`, `email`, `phoneNumber`, `password`) VALUES (14, 'mahzyar', 'mahzyar.momeni@gmail.com', '09334328703', '$2b$10$JYCNvz8FUb3tO0pNCFUTZ./8PWrsrMqiOfzPNdSwLnkeG9bEiJvXm');
