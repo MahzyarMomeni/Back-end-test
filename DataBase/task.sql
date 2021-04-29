6880278375d2af7f8016e4981f8b537ad0b84863
MahzyarMomeni <mahzyar.momeni@gmail.com>
2021-04-29 19:21:16 +0430

    master-add postman collection and database file

---
+++ DataBase/task.sql
@@ -0,0 +1,2 @@
+INSERT INTO `task` (`id`, `note`, `status`) VALUES (29, 'task', 'todo');
+INSERT INTO `task` (`id`, `note`, `status`) VALUES (33, 'task', 'done');
---
+++ DataBase/user.sql
@@ -0,0 +1,1 @@
+INSERT INTO `user` (`id`, `username`, `email`, `phoneNumber`, `password`) VALUES (1, 'Mahzyar', 'Mahzyar.momeni@gmail.com', '09334328703', '123456789');
